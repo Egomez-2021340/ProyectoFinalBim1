4b0ca63d94db08c8ceafe8659b50be5c102799b2
Esteban Gomez <egomez-2021340@kinal.edu.gt>
2024-03-25 22:34:34 -0600

    Se creo el modelo de factura

---
+++ src/invoices/invoice.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const InvoiceSchema = mongoose.Schema({
+    idUser:{
+        type:String
+    },
+    name:{
+        type:String,
+        required:[true,"El nombre es obligatorio"],
+    },
+    totalProducts:{
+        type:Array,
+        default:[]
+    },
+    totalPay:{
+        type:Number,
+        default:0
+    },
+    pay:{
+        type:Number,
+        default:0
+    },
+    turned:{
+        type:Number,
+        default:0
+    },
+    total:{
+        type:Number,
+        default:0
+    },
+    created_at:{
+        type:Date,
+        default:Date.now
+    },
+    state:{
+        type:Boolean,
+        default:true
+    }
+})
+
+
+InvoiceSchema.methods.toJSON=function(){
+    const {__v,_id,state,...invoice}=this.toObject();
+    invoice.uid=_id;
+    return invoice;
+}
+
+export default mongoose.model('Invoice',InvoiceSchema);
